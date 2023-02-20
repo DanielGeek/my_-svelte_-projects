80ed6c11fe24f4bbce3da94f60a2be21ff3f0cee
Daniel <daniel30081990@gmail.com>
2023-02-20 17:58:32 -0300

    101. Layout Load Functions

--- 02-my-sveltekit-app/src/routes/products/+page.server.ts
+++ 02-my-sveltekit-app/src/routes/products/+page.server.ts
@@ -1,7 +1,9 @@
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ parent }) => {
 	console.log('Products Load Function.');
+	const parentData = await parent();
+	console.log(parentData);
 	const products = await (await import('$lib/dummy-products.json')).default;
 	return {
 		products
