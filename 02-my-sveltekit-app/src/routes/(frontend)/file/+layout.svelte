93c97c3582ef0bf031ccef4749058a38e275d44f
Daniel <daniel30081990@gmail.com>
2023-02-19 23:08:49 -0300

    97. Shared Load Function (Server & Client)

--- 02-my-sveltekit-app/src/routes/(frontend)/file/+layout.svelte
+++
@@ -1,5 +0,0 @@
-<div style:background="#7f79d1" style:padding="10px">
-	<h3>file Layout</h3>
-
-	<slot />
-</div>
--- 02-my-sveltekit-app/src/routes/(frontend)/file/[id]/+layout.svelte
+++
@@ -1,4 +0,0 @@
-<div style:background="#d179af" style:padding="10px">
-	<h3>file/[id] Layout</h3>
-	<slot />
-</div>
