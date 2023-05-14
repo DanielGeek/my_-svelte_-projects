0f6d7af59174154159dbbb2ade52476c2ce7fa94
Daniel <daniel30081990@gmail.com>
2023-05-14 12:43:29 -0400

    192. Adding a Retry Button in Error Page

--- 03-svelte-spotify/src/routes/+error.svelte
+++ 03-svelte-spotify/src/routes/+error.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { Button, LogoutButton } from '$components';
+
+    let isRetrying = false;
+    const retryRoutes = ['/album/[id]'];
 </script>
 
 <svelte:head>
@@ -21,9 +25,24 @@
 	{#if $page.status === 401}
 		<p>Your current session has expired, please logout and login again.</p>
 		<div class="buttons">
-			<LogoutButton />
+            <div class="buttons">
+			    <LogoutButton />
+            </div>
 		</div>
 	{/if}
+
+    {#if ![404, 401].includes($page.status) && $page.route.id && retryRoutes.includes($page.route.id)}
+    <div class="buttons">
+        <Button
+            disabled={isRetrying}
+            element="button"
+            on:click={async () => {
+                isRetrying = true;
+                await invalidate(`app:${$page.route.id}`);
+                isRetrying = false;
+            }}>Retry</Button>
+    </div>
+    {/if}
 </div>
 
 <style lang="scss">
