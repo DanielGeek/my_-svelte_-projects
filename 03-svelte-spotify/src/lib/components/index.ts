838cc7c0b8925adf1c0e8a7a504fe7aa8dfe96c2
Daniel <daniel30081990@gmail.com>
2023-07-02 20:08:15 -0400

    205. Moving the Playlist Form to a Separate Component

--- 03-svelte-spotify/src/lib/components/index.ts
+++ 03-svelte-spotify/src/lib/components/index.ts
@@ -10,3 +10,4 @@ export { default as TrackList } from './TrackList.svelte';
 export { default as Player } from './Player.svelte';
 export { default as Toasts } from './Toasts.svelte';
 export { default as Pagination } from './Pagination.svelte';
+export { default as PlaylistForm } from './PlaylistForm.svelte';
