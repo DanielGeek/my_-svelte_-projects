3f8498ca790905209d7f9fe58c92eb38424a22c6
Daniel <daniel30081990@gmail.com>
2023-07-02 21:19:37 -0400

    206. Edit Playlist Page & Form Action

--- 03-svelte-spotify/src/routes/playlists/new/+page.server.ts
+++ 03-svelte-spotify/src/routes/playlists/new/+page.server.ts
@@ -1,6 +1,6 @@
-import { fail, redirect } from "@sveltejs/kit";
-import type { Actions } from "./$types";
-import { SPOTIFY_BASE_URL } from "$env/static/private";
+import { SPOTIFY_BASE_URL } from '$env/static/private';
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
 
 export const actions: Actions = {
     default: async ({ fetch, request, cookies }) => {
@@ -10,13 +10,13 @@ export const actions: Actions = {
         const description = data.get('description');
         const userID = data.get('userID');
 
-        if(!name) {
+        if (!name) {
             return fail(400, {
                 name,
                 description,
                 nameError: 'Playlist name is required!',
                 apiError: false
-            })
+            });
         }
 
         const res = await fetch(`${SPOTIFY_BASE_URL}/users/${userID}/playlists`, {
@@ -24,20 +24,20 @@ export const actions: Actions = {
             headers: {
                 Authorization: `Bearer ${cookies.get('access_token')}`
             },
-            body: JSON.stringify({name, description})
+            body: JSON.stringify({ name, description })
         });
 
-        if(!res.ok) {
+        if (!res.ok) {
             const errorJSON = await res.json();
             return fail(res.status, {
                 name,
                 description,
-                apiError: errorJSON?.error?.message ?? 'An error has ocurred',
+                apiError: errorJSON?.error?.message ?? 'An error has occurred',
                 nameError: false
-            })
+            });
         } else {
             const resJSON: SpotifyApi.CreatePlaylistResponse = await res.json();
             throw redirect(303, `/playlist/${resJSON.id}`);
         }
     }
-}
+};
